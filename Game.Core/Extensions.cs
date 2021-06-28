bab63a6e61a68de537ae06e55e914e51548f4dec
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2021-06-28 13:44:34 +0200

    Extension methods demo

---
+++ Game.Core/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Core
+{
+   public static class Extensions
+    {
+        public static string AddString(this string name, string newString)
+        {
+            return $"{name} {newString}";
+        }
+
+        internal static void Do(this string name)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
--- Game.Core/Program.cs
+++ Game.Core/Program.cs
@@ -6,12 +6,26 @@ namespace Game.Core
     {
         static void Main(string[] args)
         {
-            Game game = new Game();
-            game.Run();
+            var name = "Kalle";
+            var newString = name.AddString("Anka");
+
+            name.Do();
+            //var res2  = Extensions.AddString("Kalle", "Anka");
+            var res4 = name.AddString("Hej");
+
+            var title = "Inspector";
+            var res =  title.AddString("Gadget");
+
+            Console.WriteLine(res);
+
+            Console.WriteLine(newString);
+
+            //Game game = new Game();
+            //game.Run();
 
 
-            Console.WriteLine("Thanks for playing");
-            Console.ReadKey();
+            //Console.WriteLine("Thanks for playing");
+            //Console.ReadKey();
         }
     }
 }
