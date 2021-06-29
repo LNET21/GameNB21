d398a4269da8ea24a2f23a32d768bc8112a60596
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2021-06-29 12:00:07 +0200

    Add Test, LimitidList

---
+++ Game.LimitedList/LimitedList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Game.LimitedList
+
+{
+    public class LimitedList<T>
+    {
+        private int capacity;
+        private List<T> list;
+
+        public int Capacity => capacity;
+
+        public bool IsFull { get; set; }
+
+        public LimitedList(int capacity)
+        {
+            this.capacity = Math.Max(1, capacity);
+            list = new List<T>(this.capacity);
+        }
+
+        public bool Add(int v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Game.Tests/LimitedList/LimitedListTests.cs
@@ -0,0 +1,50 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Game.LimitedList;
+
+namespace Game.Tests.LimitedList
+{
+    [TestClass]
+    public class LimitedListTests
+    {
+        [TestMethod]
+        public void Constructor_NegativeCapacity_Returns_1()
+        {
+            //Arrange
+            const int expected = 1;
+            const int capacity = -10;
+
+            //Act
+            var list = new LimitedList<int>(capacity);
+
+            //Assert
+            Assert.AreEqual(list.Capacity, expected);
+        }
+
+        [TestMethod]
+        public void Capacity_ReturnsExcpectedCapacity_ShouldPass()
+        {
+            const int expected = 10;
+
+            var list = new LimitedList<int>(expected);
+
+            Assert.AreEqual(list.Capacity, expected);
+        }
+
+        [TestMethod]
+        public void Add_WhenFull_Should_Return_False()
+        {
+            const int capacity = 1;
+
+            var list = new LimitedList<int>(capacity);
+            var ok = list.Add(1);
+            var shouldBeFalse = list.Add(2);
+            var actual = list.IsFull;
+
+            Assert.IsTrue(ok);
+            Assert.IsFalse(shouldBeFalse);
+           // Assert.AreEqual(list.Capacity, capacity);
+            Assert.IsTrue(actual);
+            
+        }
+    }
+}
