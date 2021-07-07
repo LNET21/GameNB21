32932425b717ce51805651ed38f284d478b2b299
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2021-07-07 14:00:02 +0200

    Test IOptions Map

---
+++ Game.Tests/Map/MapTests.cs
@@ -0,0 +1,31 @@
+﻿using Game.Core.Entities;
+using Game.Core.GameWorld;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.LimitedList.Map
+{
+    [TestClass]
+    public class MapTests
+    {
+        [TestMethod]
+        public void MapConstructor_GetCorrectWidth_IOptions()
+        {
+            const int expected = 10;
+            var mapSettings = new Mapsettings { X = expected, Y = expected };
+
+            IOptions<Mapsettings> options = Options.Create(mapSettings);
+
+            var map = new ConsoleMap(options);
+
+            var actual = map.Width;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
